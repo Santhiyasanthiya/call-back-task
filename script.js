c76ad4bfe939833dac6bb59620913c015ca22ccc
Santhiyasanthiya <103623501+Santhiyasanthiya@users.noreply.github.com>
2022-08-04 20:56:42 +0530

    Add files via upload
    call back task day 17

---
+++ script.js
@@ -0,0 +1,54 @@
+// Callback Hell Task
+// Display countdown from 10 to 1 on the screen and then display  the Quotes on the screen
+
+var i=11;
+    var time=document.createElement('div');
+    time.style.fontSize="80px";
+    time.style.color="#9f9bc5";
+    setTimeout(function(){
+        time.innerText="Countdown Starts Now";
+        setTimeout(function(){
+            i--;
+            time.innerText=i;
+            setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                i--;
+                time.innerText=i;
+                setTimeout(function(){
+                time.innerText="“ Success is not final, failure is not fatal: it is the courage to continue that counts.💪”";
+                },2000)
+                },1000)
+                },1000)
+                },1000)
+                },1000)
+                },1000)
+                },1000)
+                },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+    document.body.append(time);
